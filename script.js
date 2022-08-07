1d66b14572dcec5eba5717112f04832c04196e92
Yash Sachan <yashsachan321@gmail.com>
2022-08-08 01:14:06 +0530

    app working now

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let weather = {
   apiKey: "a6c22797611f9b8feb424b36492db321",
-  fetchweather: function () {
+  fetchweather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=noida&units=metric&appid=a6c22797611f9b8feb424b36492db321"
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log("yash");
+    document.querySelector(".city").textContent = "weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").textContent=description;
+    document.querySelector(".temp")=temp+" degree celsius";
+    document.querySelector(".humidity").textContent="Humidity "+humidity+"%";
+    document.querySelector(".wind").textContent="Wind speed: "+speed+"km/hr";
   },
 };
